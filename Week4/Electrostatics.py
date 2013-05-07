e34067dafa9ab0d15c4cfc182bdd0d89874a6ceb
Raymond Mullen <rmullen@csm-pm-38123-02.local>
2013-05-07 15:59:31 -0700

    Midterm Stuff

--- Week4/Electrostatics.py
+++ Week4/Electrostatics.py
@@ -18,3 +18,15 @@ def dipolePotential(x,y,q,d):
     V2 = pointPotential(x,y,q,-abs(d),0.0)
     Vxy = V1 - V2
     return Vxy
+
+def pointField(x,y,q,Xq,Yq):
+    """Takes as arguments:
+        arrays (x,y)
+        charge q in units of Coulombs
+        position (Xq, Yq)
+
+       Returns a tuple of the electric field components (Ex, Ey)"""
+    k = 8.987551787 #(Nm^2)/C^2
+    Ex = (k * q * (x - Xq)) / ((x - Xq)**2.0 + (y - Yq)**2.0)**(1.0/2.0)
+    Ey = (k * q * (y - Yq)) / ((x - Xq)**2.0 + (y - Yq)**2.0)**(1.0/2.0)
+    return Ex, Ey
